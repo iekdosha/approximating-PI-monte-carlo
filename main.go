de599810822d6d0b7821bc1bfb4712f5a159f66d
Itzhak Eretz Kdosha <iekdosha@gmail.com>
2018-10-07 17:56:44 +0300

    Readme update & refactoring
    - Readme updated
    - Table now show sorted results
    - code refactored to match readme defenitions of terms.

--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 	"math"
 	"math/rand"
 	"strconv"
+	"time"
 )
 
 const xCenter float64 = 0.5
 const yCenter float64 = 0.5
 const radius float64 = 0.5
 const repetitions int = 1000000
+const trackInterval int = 10000
+
 
 type Point struct {
 	x float64
@@ -18,32 +21,42 @@ type Point struct {
 }
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
 	var point Point
-	count := 0
 	hit := 0
-	var track map[int]float64
-	track = make(map[int]float64)
+	miss := 0
+	var track []float64
+	// create random points
 	for i:=1 ; i <= repetitions ; i++ {
+		//generate a random point
 		point = getRandomPoint()
-		count ++;
+		// count hit or miss points
 		if(isInsideTheCircle(point)){
-			hit ++;
+			hit ++
+		} else{
+			miss ++
 		}
-		if i% 10000 == 0{
-			track[i] = float64(hit)/float64(count)/(math.Pow(radius,2.0))
+		// enter a result to the track in preset intervals
+		if i%trackInterval == 0{
+			track = append(track,calcHitRatio(hit,miss)/(math.Pow(radius,2.0)))
 		}
 	}
 
+	// create a table
 	table := tables.New([]string{"Iteration", "Evaluation"})
-
-	for key,val := range track{
-		iterString := strconv.Itoa(key)
+	// insert rows into table
+	for i,val := range track{
+		iterString := strconv.Itoa((i+1)* trackInterval)
 		evalString := strconv.FormatFloat(val, 'f', -1, 64)
 		table.AddRow(map[string]interface{}{"Iteration": iterString , "Evaluation": evalString })
 	}
-
+	// print table
 	table.Print()
-	
+
+}
+
+func calcHitRatio(hit int , miss int) float64{
+	return float64(hit)/float64(hit + miss)
 }
 
 func getRandFloat() float64{
